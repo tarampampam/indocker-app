5e90229a6449ec9221bb4591160f31d2018c9e28
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2023-02-11 19:36:54 +0400

    feat: New frontend

---
+++ frontend/src/index.ts
@@ -0,0 +1,5 @@
+import {createApp} from 'vue'
+import mainApp from './views/main-app.vue'
+
+createApp(mainApp)
+  .mount('#app')
---
+++ frontend/src/views/main-app.vue
@@ -0,0 +1,35 @@
+<template>
+  <main>
+    Hello, world!
+  </main>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  components: {},
+
+  data(): {[key: string]: unknown} {
+    return {}
+  },
+
+  created(): void {
+    // ...
+  },
+
+  computed: {
+    // ...
+  },
+
+  methods: {
+    // ...
+  },
+})
+</script>
+
+<style lang="scss">
+$web-font-path: false; // disable external font named "Lato"
+
+@import "~bootstrap/scss/bootstrap";
+</style>
