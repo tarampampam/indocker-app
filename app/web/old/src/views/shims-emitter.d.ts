ae01479e7140546fc7a1d5fae7ed15f918bbe6e3
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2023-03-17 19:58:25 +0400

    wip: 🔕 temporary commit

--- app/web/old/src/views/shims-emitter.d.ts
+++
@@ -1,8 +0,0 @@
-import {Emitter} from 'mitt' // https://github.com/developit/mitt
-
-declare module '@vue/runtime-core' {
-  // provide typings for `this.$emitter`
-  interface ComponentCustomProperties {
-    $emitter: Emitter<any>
-  }
-}
