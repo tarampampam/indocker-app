a82cb26ce520686c2ecd61c99eb7afab4d486423
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2023-03-17 16:44:40 +0400

    wip: 🔕 temporary commit

--- app/web/src/index.ts
+++ app/web/src/index.ts
@@ -1,10 +1,7 @@
 import {createApp} from 'vue'
-import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement } from 'chart.js'
 import mitt from 'mitt'
-import mainApp from './views/main-app.vue'
+import App from './views/app.vue'
 
-ChartJS.register(Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement)
-
-createApp(mainApp)
+createApp(App)
   .use((app) => app.config.globalProperties.$emitter = mitt())
   .mount('#app')
