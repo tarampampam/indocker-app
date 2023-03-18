2fab2b6f6c774761e08b42dc5f7b7dff69c47124
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2023-03-18 20:41:55 +0400

    wip: 🔕 temporary commit

---
+++ app/web/src/components/PageFooter.vue
@@ -0,0 +1,9 @@
+<template>Footer</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ app/web/src/components/ViewPreferences.vue
@@ -0,0 +1,21 @@
+<template>
+  <n-icon size="40">
+    <preferences-icon />
+  </n-icon>
+  Stats monitor
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { NIcon } from 'naive-ui'
+import { Build as PreferencesIcon } from '@vicons/ionicons5'
+
+export default defineComponent({
+  components: {
+    NIcon,
+    PreferencesIcon
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
