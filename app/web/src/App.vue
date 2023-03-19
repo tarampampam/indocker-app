9e137c6cb7d3d4266b7cce5fd0a0135c262e41b9
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2023-03-19 18:42:30 +0400

    wip: 🔕 temporary commit

--- app/web/src/App.vue
+++ app/web/src/App.vue
@@ -1,47 +1,32 @@
 <template>
   <n-config-provider :theme="theme">
-    <n-tabs
-      default-value="containers"
-      justify-content="space-around"
-      type="line"
-      :value="this.$route.name"
-    >
-      <n-tab
-        v-for="route in this.$router.getRoutes().filter((r) => r.meta.visible)"
-        v-on:click="this.$router.push({ name: route.name })"
-        :key="route.name"
-        :name="route.name"
-        :style="{padding: '2em 0 1em 0'}"
-      >
-        {{ route.meta.title }}
-      </n-tab>
-    </n-tabs>
+    <top-navigation />
     <main>
       <router-view></router-view>
     </main>
     <n-card class="footer">
       <template #header>
-        <page-footer/>
+        <page-footer />
       </template>
     </n-card>
 
-    <n-global-style/>
+    <n-global-style />
   </n-config-provider>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
-import { darkTheme, lightTheme, NCard, NConfigProvider, NGlobalStyle, NTab, NTabs } from 'naive-ui'
 import PageFooter from './components/PageFooter.vue'
+import TopNavigation from './components/TopNavigation.vue'
+import { darkTheme, lightTheme, NCard, NConfigProvider, NGlobalStyle } from 'naive-ui'
 
 export default defineComponent({
   components: {
     NConfigProvider,
     NGlobalStyle,
-    NTabs,
     NCard,
-    NTab,
     PageFooter,
+    TopNavigation
   },
   setup() {
     const dark = darkTheme
@@ -58,9 +43,9 @@ export default defineComponent({
     }
 
     return {
-      theme: theme,
+      theme: theme
     }
-  },
+  }
 })
 </script>
 
