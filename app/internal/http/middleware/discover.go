8353b741515c867c2cc48d7a73909c737fa93380
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2023-03-30 13:15:46 +0400

    ✨ feat(discover.go): add support for CORS headers and methods 🐛 fix(discover.go): change route and header names 🐛 fix(discover.go): fix scheme detection ✨ feat(discover.go): add support for dashboardDomain parameter ✅ test(discover_test.go): add tests for CORS headers and methods ✅ test(discover_test.go): add tests for wrong method and missing header ✅ test(functional.js): add tests for CORS headers and methods
    🔥 chore(healthcheck.js): remove k6 healthcheck test
    
    ✨ feat(api.ts): add discover function to fetch discover endpoint
    
    ✨ feat(main.ts): add discover function call to log discover endpoint response

--- app/internal/http/middleware/discover.go
+++ app/internal/http/middleware/discover.go
@@ -4,43 +4,65 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 // DiscoverMiddleware is a middleware that returns information about the current service.
-func DiscoverMiddleware(baseUrl string, next http.Handler) http.Handler {
+func DiscoverMiddleware(dashboardDomain string, next http.Handler) http.Handler {
 	const (
-		needRoute  = "/discover"
-		needMethod = http.MethodTrace
+		needRoute  = "/x/indocker/discover"
+		needHeader = "X-Indocker"
+		meedMethod = http.MethodGet
 	)
 
-	type response struct {
-		BaseUrl *string `json:"base_url"` // without trailing slash
+	// getScheme returns the scheme of the request.
+	var getScheme = func(r *http.Request) string {
+		if r.TLS != nil {
+			return "https"
+		}
+
+		return "http"
+	}
+
+	// writeResponse writes the response to the client.
+	var writeResponse = func(w http.ResponseWriter, r *http.Request) {
+		var data = struct {
+			API struct {
+				BaseUrl *string `json:"base_url"` // without trailing slash
+			} `json:"api"`
+		}{}
+
+		if dashboardDomain != "" {
+			u := fmt.Sprintf("%s://%s.indocker.app/api", getScheme(r), dashboardDomain)
+			data.API.BaseUrl = &u
+		}
+
+		_ = json.NewEncoder(w).Encode(data)
 	}
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == needMethod && r.URL.Path == needRoute && r.Header.Get("X-InDocker") == "true" {
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Methods", needMethod)
-			w.Header().Set("Access-Control-Allow-Headers", "X-InDocker")
-			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		if dashboardDomain != "" && r.URL.Path == needRoute {
+			w.Header().Set("Access-Control-Allow-Origin", fmt.Sprintf("%s://%s.indocker.app", getScheme(r), dashboardDomain))
+			w.Header().Set("Access-Control-Allow-Methods", meedMethod)
+			w.Header().Set("Access-Control-Allow-Headers", "*")
 
-			w.WriteHeader(http.StatusOK)
+			switch r.Method {
+			case http.MethodOptions: // https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
+				w.WriteHeader(http.StatusNoContent)
 
-			var (
-				scheme = "http"
-				data   response
-			)
+			case meedMethod: // response with data
+				if ok, _ := strconv.ParseBool(r.Header.Get(needHeader)); ok {
+					w.Header().Set("Content-Type", "application/json; charset=utf-8")
+					w.WriteHeader(http.StatusOK)
 
-			if r.TLS != nil {
-				scheme = "https"
+					writeResponse(w, r)
+				} else {
+					w.WriteHeader(http.StatusBadRequest) // missing header
+				}
+
+			default:
+				w.WriteHeader(http.StatusMethodNotAllowed)
 			}
-
-			if baseUrl != "" {
-				u := fmt.Sprintf("%s://%s.indocker.app", scheme, baseUrl)
-				data.BaseUrl = &u
-			}
-
-			_ = json.NewEncoder(w).Encode(data)
 
 			return
 		}
--- app/web/src/main.ts
+++ app/web/src/main.ts
@@ -4,7 +4,10 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 import './assets/main.css'
 import { router } from './router'
+import { discover } from '@/api/api';
 
 hljs.registerLanguage('accesslog', accesslog)
 
 createApp(App).use(router()).mount('#app')
+
+discover().then(console.log)
