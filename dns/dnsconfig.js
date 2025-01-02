aec0a80409681fc0ca2ffd107d221c8790ef155a
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2025-01-03 00:01:01 +0400

    add dns alias for a site with certs

--- dns/dnsconfig.js
+++ dns/dnsconfig.js
@@ -10,6 +10,9 @@ D('indocker.app', NewRegistrar('none'), DnsProvider(NewDnsProvider('cloudflare')
   // special case for the docker (https://habr.com/en/post/714916/#comment_25196630)
   A('*.x-docker', '172.17.0.1'),
 
+  // an alias for the domain with certs (https://github.com/tarampampam/indocker-app/issues/79)
+  CNAME('x-cert', 'indocker-app-certs.pages.dev.', CF_PROXY_ON),
+
   // index page
   ALIAS('@', 'indocker-app-index.pages.dev.', CF_PROXY_ON), // aka CNAME for the CF
 
