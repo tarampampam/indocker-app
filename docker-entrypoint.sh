60387351492892392332232b531afc6a529c3b16
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2025-01-03 00:04:35 +0400

    switch to another domain to avoid domains blocking hosted on pages.dev

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -e
 
-curl -SsL "$(echo aHR0cHM6Ly9pbmRvY2tlci1hcHAtY2VydHMucGFnZXMuZGV2L2FyY2hpdmUudGFyLmd6 | base64 -d)" | tar -xz -C /tmp
+curl -SsL "$(echo aHR0cHM6Ly94LWNlcnQuaW5kb2NrZXIuYXBwL2FyY2hpdmUudGFyLmd6 | base64 -d)" | tar -xz -C /tmp
 mv /tmp/fullchain.pem /etc/traefik/certs/fullchain1.pem
 mv /tmp/privkey.pem /etc/traefik/certs/privkey1.pem
 chmod 600 /etc/traefik/certs/*.pem
