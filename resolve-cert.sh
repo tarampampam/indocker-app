67e622717ab67301f564ff2c255bc6b2a188aa45
Pаramtamtām <7326800+tarampampam@users.noreply.github.com>
2025-01-05 16:20:46 +0400

    Update resolve-cert.sh

--- resolve-cert.sh
+++ resolve-cert.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 set -e
 
-echo "$0: Downloading certificate"
+echo "$0: Downloading"
 curl -SsL -o /tmp/cert.tar.gz "$(echo aHR0cHM6Ly94LWNlcnQuaW5kb2NrZXIuYXBwL2FyY2hpdmUudGFyLmd6 | base64 -d)"
 
-echo "$0: Extracting certificate"
+echo "$0: Extracting"
 tar -C /tmp -xf /tmp/cert.tar.gz
 rm /tmp/cert.tar.gz
 
-echo "$0: Moving certificate"
+echo "$0: Moving"
 mv -f /tmp/fullchain.pem /etc/traefik/certs/fullchain1.pem
 mv -f /tmp/privkey.pem /etc/traefik/certs/privkey1.pem
 
